b1ed81676bc59ab4afcccd61aa1d882bfebce5a5
zzy <664587718@qq.com>
2016-04-16 23:34:45 +0800

    update

--- model/__init__.py
+++ model/__init__.py
@@ -4,6 +4,8 @@ class MongoDB:
 	def __init__(self):
 		from pymongo import MongoClient
 		self.con = MongoClient('localhost')
+		admin = self.con.admin
+		admin.authenticate('zzy','caD7rovL')
 		self.db = self.con.challenge
 
 class RedisDB:
