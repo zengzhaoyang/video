4557d459396aeb19e78ace4fa9929d85d09405bd
Heliang Zheng <v-hezhen@microsoft.com>
2017-05-05 13:36:20 +0800

    test

--- web/__init__.py
+++ web/__init__.py
@@ -8,64 +8,96 @@ def index():
     headers = request.headers
     host = headers['Host']
     if host.startswith('2016'):
-        return render_template('index.2016.html')
-    elif host == 'ms-multimedia-challenge.com':
-        return redirect('http://2017.ms-multimedia-challenge.com')
-    else:
-        return render_template('index.2017.html')
+        return redirect('http://ms-multimedia-challenge.com/2016')
+    elif host.startswith('2017'):
+        return redirect('http://ms-multimedia-challenge.com/2017')
+    return redirect('/2017')
+
+@app.route('/<year>', methods=['GET'])
+def index_year(year):
+    if year != '2016' and year != '2017':
+        abort(404)
+    return render_template('index.%s.html'%year)
 
 @app.route('/people', methods=['GET'])
 def people():
     headers = request.headers
     host = headers['Host']
     if host.startswith('2016'):
-        return render_template('people.2016.html')
-    elif host == 'ms-multimedia-challenge.com':
-        return redirect('http://2017.ms-multimedia-challenge.com/people')
-    else:
-        return render_template('people.2017.html')
+        return redirect('http://ms-multimedia-challenge.com/2016/people')
+    elif host.startswith('2017'):
+        return redirect('http://ms-multimedia-challenge.com/2017/people')
+    return redirect('/2017/people')
+
+@app.route('/<year>/people', methods=['GET'])
+def people_year(year):
+    if year != '2016' and year != '2017':
+        abort(404)
+    return render_template('people.%s.html'%year)
 
 @app.route('/challenge', methods=['GET'])
 def challenge():
     headers = request.headers
     host = headers['Host']
     if host.startswith('2016'):
-        return render_template('challenge.2016.html')
-    elif host == 'ms-multimedia-challenge.com':
-        return redirect('http://2017.ms-multimedia-challenge.com/challenge')
-    else:
-        return render_template('challenge.2017.html')
+        return redirect('http://ms-multimedia-challenge.com/2016/challenge')
+    elif host.startswith('2017'):
+        return redirect('http://ms-multimedia-challenge.com/2017/challenge')
+    return redirect('/2017/challenge')
+
+@app.route('/<year>/challenge', methods=['GET'])
+def challenge_year(year):
+    if year != '2016' and year != '2017':
+        abort(404)
+    return render_template('challenge.%s.html'%year)
 
 @app.route('/dataset', methods=['GET'])
 def dataset():
     headers = request.headers
     host = headers['Host']
     if host.startswith('2016'):
-        return render_template('dataset.2016.html')
-    elif host == 'ms-multimedia-challenge.com':
-        return redirect('http://2017.ms-multimedia-challenge.com/dataset')
-    else:
-        return render_template('dataset.2017.html')
+        return redirect('http://ms-multimedia-challenge.com/2016/dataset')
+    elif host.startswith('2017'):
+        return redirect('http://ms-multimedia-challenge.com/2017/dataset')
+    return redirect('/2017/dataset')
+
+@app.route('/<year>/dataset', methods=['GET'])
+def dataset_year(year):
+    if year != '2016' and year != '2017':
+        abort(404)
+    return render_template('dataset.%s.html'%year)
 
 @app.route('/leaderboard', methods=['GET'])
 def leaderboard():
     headers = request.headers
     host = headers['Host']
     if host.startswith('2016'):
-        return render_template('leaderboard.2016.html')
-    else:
+        return redirect('http://ms-multimedia-challenge.com/2016/leaderboard')
+    elif host.startswith('2017'):
+        return redirect('http://ms-multimedia-challenge.com/2017/leaderboard')
+    return redirect('/2017/dataset')
+
+@app.route('/<year>/leaderboard', methods=['GET'])
+def leaderboard_year(year):
+    if year != '2016':
         abort(404)
+    return render_template('leaderboard.%s.html'%year)
 
 @app.route('/contact', methods=['GET'])
-def external():
+def contact():
     headers = request.headers
     host = headers['Host']
     if host.startswith('2016'):
-        return render_template('contact.2016.html')
-    elif host == 'ms-multimedia-challenge.com':
-        return redirect('http://2017.ms-multimedia-challenge.com/contact')
-    else:
-        return render_template('contact.2017.html')
+        return redirect('http://ms-multimedia-challenge.com/2016/contact')
+    elif host.startswith('2017'):
+        return redirect('http://ms-multimedia-challenge.com/2017/contact')
+    return redirect('/2017/contact')
+
+@app.route('/<year>/contact', methods=['GET'])
+def contact_year(year):
+    if year != '2016' and year != '2017':
+        abort(404)
+    return render_template('contact.%s.html'%year)
 
 @app.route('/browse', methods=['GET'])
 def browse():
